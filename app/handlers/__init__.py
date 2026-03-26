1f86571f8efafbde84c1a21774d8a7ed26d1fe8c
bcvfn7007 <bcvfn23@gmail.com>
2026-03-26 13:34:20 +0500

    settings upd

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,13 +1,13 @@
 from .base import start_command, help_command
 from .text import handle_text_message
-from .voice import handle_voice_message, show_text_callback
+from .voice import handle_voice_message
 from .settings import settings_command, settings_callbacks
 from .payments import send_invoice_callback, precheckout_callback, successful_payment_callback
 
 __all__ = [
     "start_command", "help_command",
     "handle_text_message",
-    "handle_voice_message", "show_text_callback",
+    "handle_voice_message",
     "settings_command", "settings_callbacks",
     "send_invoice_callback", "precheckout_callback", "successful_payment_callback"
 ]
